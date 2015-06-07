3f58d3c634499405a3e6e0d259de8785f4578527
Alan Carden <acarden120@gmail.com>
2015-06-07 15:14:33 +0100

    Updated Bookmark model

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,11 +1,8 @@
 class IncomingController < ApplicationController
-
-  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
     puts ">>>>>>>>>> #{params.inspect}"
-
     user = User.find_by(email: params[:sender])
     subject = params[:subject]
     content = params[:'body-plain']
@@ -16,10 +13,6 @@ class IncomingController < ApplicationController
 
     if bookmark.save
       head 200
-    else
-      head :no_content
     end
-
-#    head 200
   end
 end
