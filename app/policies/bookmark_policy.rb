3c66cf4cfc72f0d9719d60b42bb7db4e557469c6
Alan Carden <acarden120@gmail.com>
2015-06-20 17:29:23 +0100

    Complete User Authorization

--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,2 +1,9 @@
 class BookmarkPolicy < ApplicationPolicy
+  def create?
+    user.present? && (record.topic.user == user)
+  end
+
+  def destroy?
+    user.present? && (record.topic.user == user)
+  end
 end
