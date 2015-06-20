c692884f4007fccfb199080a50aebf7e68608a53
Alan Carden <acarden120@gmail.com>
2015-06-20 13:51:16 +0100

    Updated application policy

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -15,7 +15,7 @@ class ApplicationPolicy
   end
 
   def create?
-    user.present?
+    user.present? && (record.user == user)
   end
 
   def new?
@@ -31,7 +31,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-#    user.present? # && (record.user == user)
+    user.present? && (record.user == user)
   end
 
   def scope
