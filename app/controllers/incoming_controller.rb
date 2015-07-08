647f4c17a2fa3ca1ca8ebf0e15dfb52712a079d5
il1ja <dalibor.ilijevski@gmail.com>
2015-07-08 13:09:36 +0200

    merged User_Profile to master

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,8 +2,6 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-<<<<<<< HEAD
-    puts ">>>>>>>>>> #{params.inspect}"
     user = User.find_by(email: params[:sender])
     subject = params[:subject]
     content = params[:'body-plain']
@@ -12,17 +10,6 @@ class IncomingController < ApplicationController
 
     bookmark = topic.bookmarks.build(topic: topic, url: content)
 
-    if bookmark.save
-      head 200
-    end
-=======
-    user = User.find_by(email: params[:sender])
-    subject = params[:subject]
-    content = params[:'body-plain']
-    topic = user.topics.find_or_create_by(title: subject)
-    bookmark = topic.bookmarks.build(topic: topic, url: content)
-
-    bookmark.save
->>>>>>> f134157ebb70b65c4067e21a662adf5313a2700f
+    head 200 if bookmark.save
   end
 end
