a678f8a3ef7c2672b4d5438179e0a5562485ef22
Alan Carden <acarden120@gmail.com>
2015-07-09 21:26:23 +0100

    Updated bookmarks friendly_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701210009) do
+ActiveRecord::Schema.define(version: 20150709201520) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string   "url"
@@ -19,9 +19,11 @@ ActiveRecord::Schema.define(version: 20150701210009) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "slug"
   end
 
   add_index "bookmarks", ["id"], name: "index_bookmarks_on_id", unique: true
+  add_index "bookmarks", ["slug"], name: "index_bookmarks_on_slug"
   add_index "bookmarks", ["topic_id"], name: "index_bookmarks_on_topic_id"
 
   create_table "likes", force: :cascade do |t|
