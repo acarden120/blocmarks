6eaf4d9408831e856a34c896e9a28a98018af318
Alan Carden <acarden120@gmail.com>
2015-07-12 19:26:17 +0100

    Complete URL preview

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,2 @@
 module UsersHelper
-  require 'embedly'
-  require 'json'
-
-  def get_preview(_url)
-    embedly_api = Embedly::API.new { key: 'd4ac3dd9d08c4b248f85e4225483471d' },
-        { user_agent: 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)' }
-    obj = embedly_api.oembed :url => url
-    json_obj = JSON.pretty_generate(obj[0].marshal_dump)
-  end
 end
