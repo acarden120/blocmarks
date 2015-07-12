30ed9a9ce91660eaf252ee53625b8d4cf0cd75b5
Alan Carden <acarden120@gmail.com>
2015-07-12 18:21:21 +0100

    Updated URL Preview

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,9 @@
 class UsersController < ApplicationController
   include Pundit
-  require 'embedly'
-  require 'json'
 
   def show
     user = current_user
     @user_bookmarks = user.topics.all
     @liked_bookmarks = user.likes
   end
-
-  def get_preview(url)
-    embedly_api =
-    Embedly::API.new :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
-
-    # single url
-#    obj = embedly_api.oembed :url => 'http://www.youtube.com/watch?v=sPbJ4Z5D-n4&feature=topvideos'
-    obj = embedly_api.oembed :url => 'http://www.youtube.com/watch?v=sPbJ4Z5D-n4&feature=topvideos'
-    puts "obj.marshal dump: #{obj[0].marshal_dump}"
-    json_obj = JSON.pretty_generate(obj[0].marshal_dump)
-    puts "json_obj: #{json_obj}"  
-  end
-
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,11 @@
 module UsersHelper
-  def get_preview(url)
-#    embedly_api =
-#    Embedly::API.new :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
-     embedly_api = Embedly::API.new :key => 'd4ac3dd9d08c4b248f85e4225483471d',
-        :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
+  require 'embedly'
+  require 'json'
 
-    # single url
-#    obj = embedly_api.oembed :url => 'http://www.youtube.com/watch?v=sPbJ4Z5D-n4&feature=topvideos'
+  def get_preview(_url)
+    embedly_api = Embedly::API.new { key: 'd4ac3dd9d08c4b248f85e4225483471d' },
+        { user_agent: 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)' }
     obj = embedly_api.oembed :url => url
-    puts obj[0].marshal_dump
     json_obj = JSON.pretty_generate(obj[0].marshal_dump)
-    puts json_obj  
   end
 end
